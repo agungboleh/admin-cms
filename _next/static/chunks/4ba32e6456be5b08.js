6bc85b389059405e365d2885ed3950a48a9dd904
agungbayu <116628319+agungboleh@users.noreply.github.com>
2025-11-27 09:55:16 +0700

    Add files via upload

---
+++ _next/static/chunks/4ba32e6456be5b08.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,18566,(e,t,o)=>{t.exports=e.r(76562)},31713,e=>{"use strict";var t=e.i(71645),o=e.i(18566);function u(){let e=(0,o.useRouter)();return(0,t.useEffect)(()=>{localStorage.getItem("isLoggedIn")?e.push("/dashboard"):e.push("/login")},[e]),null}e.s(["default",()=>u])}]);
